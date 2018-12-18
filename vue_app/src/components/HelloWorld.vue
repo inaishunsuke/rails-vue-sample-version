cfc5eb3520c88eed875f186e6d706d3a76d20e67
Inai <37035249+inaishunsuke@users.noreply.github.com>
2018-12-19 01:16:55 +0900

    Vueサンプル
    Vueのサンプルアプリ

---
+++ vue_app/src/components/HelloWorld.vue
@@ -0,0 +1,151 @@
+<template>
+  <div class="hello">
+  <p>Rails : {{results.rails}}</p>
+  <p>Ruby : {{results.ruby}}</p>
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+        >
+          Core Docs
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+        >
+          Forum
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+        >
+          Community Chat
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a
+          href="http://router.vuejs.org/"
+          target="_blank"
+        >
+          vue-router
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vuex.vuejs.org/"
+          target="_blank"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vue-loader.vuejs.org/"
+          target="_blank"
+        >
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+        >
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      results: []
+    }
+  },
+  mounted() {
+    axios.get("http://localhost:3000/version")
+    .then(response => {this.results = response.data})
+
+/*    axios("http://localhost:3000/version", {
+      method: 'GET',
+      mode: 'no-cors',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      },
+      credentials: 'same-origin',
+    })
+    .then(response => {this.results = response.data})
+*/
+    /*
+    this.$http({
+      params: "",
+      url: "http://localhost:3000/version",
+      method: 'GET'
+//      credentials: true
+    })
+    .then(
+      (response)=> {
+        // success
+        console.log(response);
+      },
+      (response)=> {
+        // error
+        console.log("error");
+        console.log(response);
+      }
+    );
+    */
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
